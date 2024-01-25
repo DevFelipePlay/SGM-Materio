90b74c004e47ff836f7194ca7f9409484260733c
devjoaoplay <joao.silva@playmovel.com.br>
2024-01-25 11:30:05 -0300

    reset

--- src/components/RegisterMultiSteps/StepDadosPessoais.tsx
+++ src/components/RegisterMultiSteps/StepDadosPessoais.tsx
@@ -1,213 +1,302 @@
-// ** React Imports
-import { useState, ChangeEvent } from 'react'
-
 // ** MUI Components
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
-import Tooltip from '@mui/material/Tooltip'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import FormControl from '@mui/material/FormControl'
-import InputAdornment from '@mui/material/InputAdornment'
-
-// ** Third Party Imports
-import Payment from 'payment'
-
-// ** Type Import
-import { CustomRadioIconsData } from 'src/@core/components/custom-radio/types'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
-// ** Custom Components Imports
-import CustomRadioIcons from 'src/@core/components/custom-radio/icons'
-
-// ** Util Import
-import { formatCVC, formatExpirationDate, formatCreditCardNumber } from 'src/@core/utils/format'
-
 // ** Styles Import
 import 'react-credit-cards/es/styles-compiled.css'
+import { TextField } from '@mui/material'
 
-const data: CustomRadioIconsData[] = [
-  {
-    value: 'basic',
-    title: <Typography variant='h5'>Basic</Typography>,
-    content: (
-      <Box sx={{ my: 'auto', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-        <Typography sx={{ textAlign: 'center', color: 'text.secondary' }}>
-          A simple start for start ups & Students
-        </Typography>
-        <Box sx={{ mt: 2, display: 'flex' }}>
-          <Typography component='sup' sx={{ mt: 1.5, color: 'primary.main', alignSelf: 'flex-start' }}>
-            $
-          </Typography>
-          <Typography component='span' sx={{ fontSize: '2rem', color: 'primary.main' }}>
-            0
-          </Typography>
-          <Typography component='sub' sx={{ mb: 1.5, alignSelf: 'flex-end', color: 'text.disabled' }}>
-            /month
-          </Typography>
-        </Box>
-      </Box>
-    )
-  },
-  {
-    isSelected: true,
-    value: 'standard',
-    title: <Typography variant='h5'>Standard</Typography>,
-    content: (
-      <Box sx={{ my: 'auto', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-        <Typography sx={{ textAlign: 'center', color: 'text.secondary' }}>For small to medium businesses</Typography>
-        <Box sx={{ mt: 2, display: 'flex' }}>
-          <Typography component='sup' sx={{ mt: 1.5, color: 'primary.main', alignSelf: 'flex-start' }}>
-            $
-          </Typography>
-          <Typography component='span' sx={{ fontSize: '2rem', fontWeight: 500, color: 'primary.main' }}>
-            99
-          </Typography>
-          <Typography component='sub' sx={{ mb: 1.5, alignSelf: 'flex-end', color: 'text.disabled' }}>
-            /month
-          </Typography>
-        </Box>
-      </Box>
-    )
-  },
-  {
-    value: 'enterprise',
-    title: <Typography variant='h5'>Enterprise</Typography>,
-    content: (
-      <Box sx={{ my: 'auto', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-        <Typography sx={{ textAlign: 'center', color: 'text.secondary' }}>
-          Solution for enterprise & organizations
-        </Typography>
-        <Box sx={{ mt: 2, display: 'flex' }}>
-          <Typography component='sup' sx={{ mt: 1.5, color: 'primary.main', alignSelf: 'flex-start' }}>
-            $
-          </Typography>
-          <Typography component='span' sx={{ fontSize: '2rem', color: 'primary.main' }}>
-            499
-          </Typography>
-          <Typography component='sub' sx={{ mb: 1.5, alignSelf: 'flex-end', color: 'text.disabled' }}>
-            /month
-          </Typography>
-        </Box>
-      </Box>
-    )
-  }
-]
+// ** Utils
+import * as z from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { maskCelular, maskCep, maskCnpj, maskCpf, maskNascimento } from 'src/utils/masks/masks'
+import { unmask } from 'remask'
+import apiCep from 'src/services/apiCep'
+import toast from 'react-hot-toast'
+
+// Schema Zod
+const SchemaFormCadastroUsuario = z.object({
+  cpf: z.string().min(1, 'CPF é obrigatório'),
+  cnpjParceiro: z.string().min(1, 'CNPJ é obrigatório'),
+  email: z.string().email('Email é obrigatório'),
+  phone: z.string().min(1, 'Celular é obrigatório'),
+  whats: z.string().min(1, 'WhatsApp é obrigatório'),
+  name: z.string().min(1, 'Nome é obrigatório'),
+  cep: z.string().min(1, 'CEP é obrigatório'),
+  uf: z.string().min(1, 'UF é obrigatório'),
+  rgie: z.string(),
+  nascimento: z.string().min(1, 'Data de Nascimento é obrigatório'),
+  cidade: z.string().min(1, 'Cidade é obrigatório'),
+  district: z.string().min(1, 'Bairro é obrigatório'),
+  street: z.string().min(1, 'Endereço'),
+  number: z.string().min(1, 'Número é obrigatório'),
+  complement: z.string()
+})
+
+// Tipagem automática de acordo com o Schema do Zod
+type FormCadastroUsuario = z.infer<typeof SchemaFormCadastroUsuario>
 
 const StepDadosPessoais = ({ handlePrev }: { handlePrev: () => void }) => {
-  const initialSelected: string = data.filter(item => item.isSelected)[data.filter(item => item.isSelected).length - 1]
-    .value
+  // React Hook Form
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors, isSubmitting }
+  } = useForm<FormCadastroUsuario>({
+    resolver: zodResolver(SchemaFormCadastroUsuario)
+  })
 
-  // ** State
-  const [cvc, setCvc] = useState<string>('')
-  const [name, setName] = useState<string>('')
-  const [expiry, setExpiry] = useState<string>('')
-  const [cardNumber, setCardNumber] = useState<string>('')
-  const [selectedRadio, setSelectedRadio] = useState<string>(initialSelected)
+  function handleSubmitCadastroUsuario(data: any) {
+    console.log(data)
+  }
 
-  const handleInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    if (target.name === 'cardNumber') {
-      target.value = formatCreditCardNumber(target.value, Payment)
-      setCardNumber(target.value)
-    } else if (target.name === 'expiry') {
-      target.value = formatExpirationDate(target.value)
-      setExpiry(target.value)
-    } else if (target.name === 'cvc') {
-      target.value = formatCVC(target.value, cardNumber, Payment)
-      setCvc(target.value)
+  // Máscaras
+  const cpfValue = watch('cpf')
+  const cnpjParceiroValue = watch('cnpjParceiro')
+  const nascimentoValue = watch('nascimento')
+  const phoneValue = watch('phone')
+  const whatsAppValue = watch('whats')
+  const cepValue = watch('cep')
+
+  useEffect(() => {
+    setValue('cpf', maskCpf(cpfValue))
+    setValue('cnpjParceiro', maskCnpj(cnpjParceiroValue))
+    setValue('nascimento', maskNascimento(nascimentoValue))
+    setValue('phone', maskCelular(phoneValue))
+    setValue('whats', maskCelular(whatsAppValue))
+    setValue('cep', maskCep(cepValue))
+  }, [setValue, cpfValue, cnpjParceiroValue, nascimentoValue, phoneValue, whatsAppValue, cepValue])
+
+  // Buscar CEP
+
+  async function getCepInfo() {
+    const formatedCep = unmask(cepValue)
+    try {
+      const data = (await apiCep.get('/' + formatedCep + '/json')).data
+      setValue('uf', data.uf)
+      setValue('cidade', data.localidade)
+      setValue('district', data.bairro)
+      setValue('street', data.logradouro)
+    } catch (error: any) {
+      toast.error('Cep Inválido')
     }
   }
 
-  const handleRadioChange = (prop: string | ChangeEvent<HTMLInputElement>) => {
-    if (typeof prop === 'string') {
-      setSelectedRadio(prop)
-    } else {
-      setSelectedRadio((prop.target as HTMLInputElement).value)
+  useEffect(() => {
+    if (cepValue && unmask(cepValue).length >= 8) {
+      getCepInfo()
     }
-  }
+  }, [cepValue])
 
   return (
     <>
       <Box sx={{ mb: 4 }}>
-        <Typography variant='h5'>Select Plan</Typography>
-        <Typography sx={{ color: 'text.secondary' }}>Select plan as per your requirement</Typography>
+        <Typography variant='h5'>Cadastre seus Dados</Typography>
+        <Typography sx={{ color: 'text.secondary' }}>Insira suas informações pessoais</Typography>
       </Box>
 
-      <Grid container spacing={5}>
-        {data.map((item, index) => (
-          <CustomRadioIcons
-            key={index}
-            data={data[index]}
-            selected={selectedRadio}
-            name='custom-radios-plan'
-            gridProps={{ sm: 4, xs: 12 }}
-            handleChange={handleRadioChange}
-          />
-        ))}
-
-        <Grid item xs={12} sx={{ pt: theme => `${theme.spacing(11.5)} !important` }}>
-          <Typography variant='h5'>Payment Information</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>Enter your card information</Typography>
-        </Grid>
+      <Grid component='form' container spacing={5} onSubmit={handleSubmit(handleSubmitCadastroUsuario)}>
         <Grid item xs={12}>
-          <FormControl fullWidth>
-            <TextField
-              fullWidth
-              name='cardNumber'
-              value={cardNumber}
-              autoComplete='off'
-              label='Card Number'
-              onChange={handleInputChange}
-              placeholder='0000 0000 0000 0000'
-            />
-          </FormControl>
+          <TextField
+            fullWidth
+            label='Nome*'
+            {...register('name')}
+            error={!!errors.name}
+            helperText={errors.name?.message}
+          />
         </Grid>
+
         <Grid item xs={12} sm={6}>
           <TextField
             fullWidth
-            name='name'
-            value={name}
-            autoComplete='off'
-            label='Name on Card'
-            placeholder='John Doe'
-            onChange={e => setName(e.target.value)}
+            placeholder='000.000.000-00'
+            label='CPF*'
+            {...register('cpf')}
+            error={!!errors.cpf}
+            helperText={errors.cpf?.message}
           />
         </Grid>
-        <Grid item xs={6} sm={3}>
+
+        <Grid item xs={12} sm={6}>
           <TextField
             fullWidth
-            name='expiry'
-            label='Expiry'
-            value={expiry}
-            placeholder='MM/YY'
-            onChange={handleInputChange}
-            inputProps={{ maxLength: '5' }}
+            placeholder='00.000.000/0000-00'
+            label='CNPJ*'
+            {...register('cnpjParceiro')}
+            error={!!errors.cnpjParceiro}
+            helperText={errors.cnpjParceiro?.message}
           />
         </Grid>
-        <Grid item xs={6} sm={3}>
+
+        <Grid item xs={12} sm={6}>
           <TextField
             fullWidth
-            name='cvc'
-            label='CVC'
-            value={cvc}
-            autoComplete='off'
-            onChange={handleInputChange}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position='start' sx={{ '& svg': { cursor: 'pointer' } }}>
-                  <Tooltip title='Card Verification Value'>
-                    <Box sx={{ display: 'flex' }}>
-                      <Icon icon='mdi:help-circle-outline' fontSize={20} />
-                    </Box>
-                  </Tooltip>
-                </InputAdornment>
-              )
-            }}
+            label='RG'
+            {...register('rgie')}
+            error={!!errors.rgie}
+            helperText={errors.rgie?.message}
           />
         </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            placeholder='dd/mm/yyyy'
+            label='Data de Nascimento*'
+            {...register('nascimento')}
+            error={!!errors.nascimento}
+            helperText={errors.nascimento?.message}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            placeholder='seu@email.com'
+            label='Email*'
+            {...register('email')}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label='Celular*'
+            placeholder='(00) 0 0000-0000'
+            {...register('phone')}
+            error={!!errors.phone}
+            helperText={errors.phone?.message}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label='WhatsApp*'
+            placeholder='(00) 0000-0000'
+            {...register('whats')}
+            error={!!errors.whats}
+            helperText={errors.whats?.message}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <TextField
+            fullWidth
+            label='CEP*'
+            placeholder='00000-000'
+            {...register('cep')}
+            error={!!errors.cep}
+            helperText={errors.cep?.message}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Controller
+            name='uf'
+            control={control}
+            defaultValue=''
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label='UF*'
+                placeholder='Ex: DF'
+                error={!!errors.uf}
+                helperText={errors.uf?.message}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Controller
+            name='cidade'
+            control={control}
+            defaultValue=''
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label='Cidade*'
+                placeholder='Ex: Brasília'
+                error={!!errors.uf}
+                helperText={errors.uf?.message}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Controller
+            name='district'
+            control={control}
+            defaultValue=''
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label='Bairro*'
+                placeholder=''
+                error={!!errors.district}
+                helperText={errors.district?.message}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Controller
+            name='street'
+            control={control}
+            defaultValue=''
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label='Logradouro*'
+                placeholder='Ex: Viela 16'
+                error={!!errors.street}
+                helperText={errors.street?.message}
+                autoComplete='off'
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <TextField
+            fullWidth
+            label='Número*'
+            placeholder='Ex: 123'
+            {...register('number')}
+            error={!!errors.number}
+            helperText={errors.number?.message}
+            type='number'
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label='Complemento'
+            placeholder='Ex: Sobrado verde ao lado da sorveteria'
+            {...register('complement')}
+          />
+        </Grid>
+
         <Grid item xs={12}>
           <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Button
@@ -216,10 +305,10 @@ const StepDadosPessoais = ({ handlePrev }: { handlePrev: () => void }) => {
               onClick={handlePrev}
               startIcon={<Icon icon='mdi:chevron-left' fontSize={20} />}
             >
-              Previous
+              Voltar
             </Button>
-            <Button color='success' variant='contained' onClick={() => alert('Submitted..!!')}>
-              Submit
+            <Button color='success' variant='contained' type='submit' disabled={isSubmitting}>
+              {isSubmitting ? 'Enviando...' : 'Enviar'}
             </Button>
           </Box>
         </Grid>
