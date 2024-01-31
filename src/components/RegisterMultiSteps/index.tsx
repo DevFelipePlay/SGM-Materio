75c6cb100c224129d6b318d1f13cfb58c782d129
devjoaoplay <joao.silva@playmovel.com.br>
2024-01-31 09:07:46 -0300

    fix tela cadastro parceiro

--- src/components/RegisterMultiSteps/index.tsx
+++ src/components/RegisterMultiSteps/index.tsx
@@ -50,11 +50,11 @@ const RegisterMultiSteps = () => {
   const getStepContent = (step: number) => {
     switch (step) {
       case 0:
-        return <StepCadastrarMVNO handleNext={handleNext} handlePrev={handlePrev} />
-      case 1:
-        return <StepDadosPessoais handlePrev={handlePrev} />
-      case 2:
         return <StepTokenPlay handleNext={handleNext} />
+      case 1:
+        return <StepCadastrarMVNO handleNext={handleNext} handlePrev={handlePrev} />
+      case 2:
+        return <StepDadosPessoais handlePrev={handlePrev} />
 
       default:
         return null
