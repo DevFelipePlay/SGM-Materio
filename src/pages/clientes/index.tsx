c8943f2fdbb88e032aa3c0ee60559741b9e177f8
devjoaoplay <joao.silva@playmovel.com.br>
2024-02-01 08:38:06 -0300

    AJUSTE VISUAL - DATA GRID

--- src/pages/clientes/index.tsx
+++ src/pages/clientes/index.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useState, useCallback, MouseEvent } from 'react'
+import { useState, useCallback } from 'react'
 
 // ** Next Imports
 
@@ -12,7 +12,7 @@ import CardHeader from '@mui/material/CardHeader'
 import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
 import CardContent from '@mui/material/CardContent'
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { DataGrid, GridColDef, gridClasses } from '@mui/x-data-grid'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 
 // ** Icon Imports
@@ -26,132 +26,31 @@ import CardStatisticsHorizontal from 'src/@core/components/card-statistics/card-
 // ** Custom Table Components Imports
 import TableHeader from 'src/views/apps/user/list/TableHeader'
 import AddUserDrawer from 'src/views/apps/user/list/AddUserDrawer'
-import { Box, IconButton, Menu, Tooltip } from '@mui/material'
-import { UsersType } from 'src/types/apps/userTypes'
-
-interface CellType {
-  row: UsersType
-}
-
-const RowOptions = ({ id }: { id: number | string }) => {
-  console.log(id)
-
-  // ** State
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-
-  const rowOptionsOpen = Boolean(anchorEl)
-
-  const handleRowOptionsClick = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleRowOptionsClose = () => {
-    setAnchorEl(null)
-  }
-
-  return (
-    <>
-      <IconButton size='small' onClick={handleRowOptionsClick}>
-        <Icon icon='mdi:dots-vertical' fontSize={20} />
-      </IconButton>
-      <Menu keepMounted anchorEl={anchorEl} open={rowOptionsOpen} onClose={handleRowOptionsClose}>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='mdi:chart-pie' fontSize={20} />
-          Consumo
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }}>
-          <Icon icon='heroicons-solid:collection' fontSize={20} />
-          Alterar Plano
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }}>
-          <Icon icon='mdi:plus-circle' fontSize={20} />
-          Recarga Adicional
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='mdi:swap-horizontal' fontSize={20} />
-          Portabilidade
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='material-symbols:paid' fontSize={20} />
-          Cobranças
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='mdi:autorenew' fontSize={20} />
-          Recorrência
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='mdi:cancel' fontSize={20} />
-          Bloqueio de Linha
-        </MenuItem>
-        <MenuItem sx={{ '& svg': { mr: 2 } }} href='/apps/user/view/overview/'>
-          <Icon icon='mdi:file-plus' fontSize={20} />
-          Adicionar Protocolo
-        </MenuItem>
-      </Menu>
-    </>
-  )
-}
 
 const columns: GridColDef[] = [
   {
-    flex: 0.15,
+    flex: 0.1,
     minWidth: 100,
     field: 'name',
     headerName: 'Nome'
   },
   {
-    flex: 0.15,
+    flex: 0.1,
     minWidth: 100,
     field: 'cpf',
     headerName: 'CPF'
   },
   {
-    flex: 0.2,
+    flex: 0.1,
     field: 'iccid',
     minWidth: 100,
     headerName: 'ICCID'
   },
   {
-    flex: 0.15,
+    flex: 0.1,
     minWidth: 100,
     field: 'msisdn',
     headerName: 'MSISDN'
-  },
-  {
-    flex: 0.1,
-    minWidth: 100,
-    field: 'mvno',
-    headerName: 'MVNO'
-  },
-  {
-    flex: 0.1,
-    minWidth: 100,
-    sortable: false,
-    field: 'tipo',
-    headerName: 'Tipo'
-  },
-  {
-    flex: 0.15,
-    minWidth: 100,
-    sortable: false,
-    field: 'actions',
-    headerName: 'Ações',
-    renderCell: ({ row }: CellType) => {
-      return (
-        <Box display='flex' alignItems='center' gap={2}>
-          <Tooltip title='Visualizar'>
-            <IconButton size='small'>
-              <Icon icon='mdi:eye-outline' fontSize={20} />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title='Excluir Linha'>
-            <IconButton size='small'>
-              <Icon icon='mdi:delete-outline' fontSize={20} />
-            </IconButton>
-          </Tooltip>
-          <RowOptions id={row.id} />
-        </Box>
-      )
-    }
   }
 ]
 
@@ -161,117 +60,91 @@ const rows = [
     name: 'Adam Sandler',
     cpf: '050.018.691-84',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 2,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 3,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 4,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 5,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 6,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 7,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 8,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 9,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 10,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 11,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 12,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   },
   {
     id: 13,
     name: 'João Pedro',
     cpf: '080.070.691-94',
     iccid: '8955170110333028510',
-    msisdn: '(61) 92004-6246',
-    mvno: 'PLAY MÓVEL',
-    tipo: 'Pré-pago'
+    msisdn: '(61) 92004-6246'
   }
 ]
 
@@ -330,7 +203,7 @@ const Clientes = () => {
         </Grid>
       </Grid>
       <Grid item xs={12}>
-        <Card>
+        <Card sx={{ p: 6 }}>
           <CardHeader title='Filtrar' />
           <CardContent>
             <Grid container spacing={6}>
@@ -406,6 +279,23 @@ const Clientes = () => {
             pageSizeOptions={[10, 25, 50]}
             paginationModel={paginationModel}
             onPaginationModelChange={setPaginationModel}
+            getRowSpacing={params => ({
+              top: params.isFirstVisible ? 10 : 5,
+              bottom: params.isLastVisible ? 10 : 5
+            })}
+            sx={{
+              [`& .${gridClasses.row}`]: {
+                bgcolor: theme => (theme.palette.mode === 'light' ? '#f1f1f1' : '#404040'),
+                borderRadius: '8px'
+              },
+              [`& .${gridClasses.cell}`]: {
+                padding: '16px',
+                border: 'none'
+              },
+              [`& .${gridClasses.columnHeader}`]: {
+                border: 'none'
+              }
+            }}
           />
         </Card>
       </Grid>
