2b93b8c73d760ec710e4c0a64d1120a9b5d9c83d
devjoaoplay <joao.silva@playmovel.com.br>
2024-02-09 10:37:42 -0300

    implementando hook form nos formularios

--- src/views/clientes/AdicionarProtocoloCliente.tsx
+++ src/views/clientes/AdicionarProtocoloCliente.tsx
@@ -1,4 +1,5 @@
 // ** MUI Imports
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Icon } from '@iconify/react'
 import {
   Box,
@@ -7,17 +8,64 @@ import {
   CardContent,
   CardHeader,
   FormControl,
+  FormControlLabel,
+  FormHelperText,
   InputLabel,
   MenuItem,
   OutlinedInput,
+  Radio,
+  RadioGroup,
   Select,
   TextField,
   Tooltip,
   Typography
 } from '@mui/material'
 import Grid from '@mui/material/Grid'
+import { useEffect } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { maskCelular } from 'src/utils/masks/masks'
+import * as z from 'zod'
+
+// ** Schema Zod
+const SchemaAdicionarProtocoloClienteForm = z.object({
+  telefoneCliente: z.string().min(1, 'Telefone do Cliente é obrigatório'),
+  mvno: z.string().min(1, 'MVNO é obrigatório'),
+  servico: z.string().min(1, 'Serviço é obrigatório'),
+  descricao: z.string().min(1, 'Descrição é obrigatório'),
+  tipoProtocolo: z.enum(['auvo', 'surf']),
+  codigo: z.string().min(1, 'é obrigatório'),
+  link: z.string().min(1, 'é obrigatório')
+})
+
+type AdicionarProtocoloClienteFormData = z.infer<typeof SchemaAdicionarProtocoloClienteForm>
 
 const AdicionarProtocoloCliente = () => {
+  // ** Hooks
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    control,
+    formState: { errors }
+  } = useForm<AdicionarProtocoloClienteFormData>({
+    resolver: zodResolver(SchemaAdicionarProtocoloClienteForm)
+  })
+
+  const tipoProtocolo = watch('tipoProtocolo')
+
+  // Máscaras
+  const telefoneClienteValue = watch('telefoneCliente')
+
+  useEffect(() => {
+    setValue('telefoneCliente', maskCelular(telefoneClienteValue))
+  }, [telefoneClienteValue, setValue])
+
+  // ** Submit Form Protocolo
+  function handleSubmitProtocoloForm(data: AdicionarProtocoloClienteFormData) {
+    console.log(data)
+  }
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
@@ -36,17 +84,27 @@ const AdicionarProtocoloCliente = () => {
             }
           />
           <CardContent>
-            <form onSubmit={e => e.preventDefault()}>
+            <form onSubmit={handleSubmit(handleSubmitProtocoloForm)}>
               <Grid container spacing={6}>
                 <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth size='medium'>
+                  <FormControl fullWidth size='medium' error={!!errors.telefoneCliente}>
                     <InputLabel htmlFor='telefone-cliente'>Telefone Cliente*</InputLabel>
-                    <OutlinedInput label='Telefone Cliente*' id='telefone-cliente' placeholder='(00) 0 0000-0000' />
+                    <OutlinedInput
+                      label='Telefone Cliente*'
+                      id='telefone-cliente'
+                      placeholder='(00) 0 0000-0000'
+                      {...register('telefoneCliente')}
+                    />
+                    {!!errors.telefoneCliente && (
+                      <FormHelperText sx={{ color: theme => theme.palette.error.main }}>
+                        {errors.telefoneCliente.message}
+                      </FormHelperText>
+                    )}
                   </FormControl>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth>
+                  <FormControl fullWidth error={!!errors.mvno}>
                     <InputLabel id='mvno-select'>MVNO*</InputLabel>
                     <Select
                       fullWidth
@@ -54,6 +112,7 @@ const AdicionarProtocoloCliente = () => {
                       label='MVNO*'
                       labelId='mvno-select'
                       inputProps={{ placeholder: 'Selecione o Tipo de Cliente' }}
+                      {...register('mvno')}
                     >
                       <MenuItem value='playmovel'>PLAY MÓVEL</MenuItem>
                       <MenuItem value='tegg'>TEGG</MenuItem>
@@ -70,11 +129,16 @@ const AdicionarProtocoloCliente = () => {
                       <MenuItem value='wipi'>WIPI</MenuItem>
                       <MenuItem value='agil'>AGIL</MenuItem>
                     </Select>
+                    {!!errors.mvno && (
+                      <FormHelperText sx={{ color: theme => theme.palette.error.main }}>
+                        {errors.mvno.message}
+                      </FormHelperText>
+                    )}
                   </FormControl>
                 </Grid>
 
                 <Grid item xs={12}>
-                  <FormControl fullWidth>
+                  <FormControl fullWidth error={!!errors.servico}>
                     <InputLabel id='servico-select'>Serviço*</InputLabel>
                     <Select
                       fullWidth
@@ -82,33 +146,91 @@ const AdicionarProtocoloCliente = () => {
                       label='Serviço*'
                       labelId='servico-select'
                       inputProps={{ placeholder: 'Selecione o Tipo de Cliente' }}
+                      {...register('servico')}
                     >
-                      <MenuItem value='1'>AJUSTE DE BENEFICIOS (DADOS, VOZ E SMS)</MenuItem>
-                      <MenuItem value='2'>ATIVAÇÃO DE CHIP</MenuItem>
-                      <MenuItem value='3'>BLOQUEIO/DESBLOQUEIO DE CHIP</MenuItem>
-                      <MenuItem value='4'>ERRO DE PORTABILIDADE</MenuItem>
-                      <MenuItem value='5'>TROCA DE CHIP</MenuItem>
+                      <MenuItem value='ajusteBeneficio'>AJUSTE DE BENEFICIOS (DADOS, VOZ E SMS)</MenuItem>
+                      <MenuItem value='ATIVAÇÃO DE CHIP'>ATIVAÇÃO DE CHIP</MenuItem>
+                      <MenuItem value='BLOQUEIO/DESBLOQUEIO DE CHIP'>BLOQUEIO/DESBLOQUEIO DE CHIP</MenuItem>
+                      <MenuItem value='ERRO DE PORTABILIDADE'>ERRO DE PORTABILIDADE</MenuItem>
+                      <MenuItem value='TROCA DE CHIP'>TROCA DE CHIP</MenuItem>
                     </Select>
+                    {!!errors.servico && (
+                      <FormHelperText sx={{ color: theme => theme.palette.error.main }}>
+                        {errors.servico.message}
+                      </FormHelperText>
+                    )}
                   </FormControl>
                 </Grid>
 
                 <Grid item xs={12}>
                   <FormControl fullWidth>
-                    <TextField multiline maxRows={4} label='Descrição' id='descricao' />
+                    <Controller
+                      name='descricao'
+                      control={control}
+                      defaultValue=''
+                      render={({ field }) => {
+                        return (
+                          <TextField
+                            {...field}
+                            multiline
+                            id='descricao'
+                            label='Descrição*'
+                            error={!!errors.descricao}
+                            helperText={errors.descricao?.message}
+                          />
+                        )
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography fontSize='1.125rem' fontWeight={500} sx={{ mb: 2 }}>
+                    Tipo Protocolo
+                  </Typography>
+                  <FormControl sx={{ flexWrap: 'wrap', flexDirection: 'row' }}>
+                    <RadioGroup defaultValue='surf' row aria-label='type-protocol-radio' {...register('tipoProtocolo')}>
+                      <FormControlLabel value='surf' control={<Radio />} label='Surf' />
+                      <FormControlLabel value='auvo' control={<Radio />} label='Auvo' />
+                    </RadioGroup>
                   </FormControl>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth size='medium'>
-                    <InputLabel htmlFor='numero-spec'>Número Spec</InputLabel>
-                    <OutlinedInput label='Número Spec' id='numero-spec' placeholder='Número do Spec da Surf' />
+                  <FormControl fullWidth size='medium' error={!!errors.codigo}>
+                    <InputLabel htmlFor={tipoProtocolo === 'surf' ? 'numero-spec' : 'numero-auvo'}>
+                      {tipoProtocolo === 'surf' ? 'Número Spec*' : 'Número Ticket'}
+                    </InputLabel>
+                    <OutlinedInput
+                      label={tipoProtocolo === 'surf' ? 'Número Spec*' : 'Número Ticket'}
+                      id={tipoProtocolo === 'surf' ? 'numero-spec' : 'numero-auvo'}
+                      placeholder={tipoProtocolo === 'surf' ? 'Número do Spec da Surf' : 'Número do Ticket do Auvo'}
+                      {...register('codigo')}
+                    />
+                    {!!errors.codigo && (
+                      <FormHelperText sx={{ color: theme => theme.palette.error.main }}>
+                        {tipoProtocolo === 'surf' ? 'Número Spec' : 'Número Ticket'} {errors.codigo.message}
+                      </FormHelperText>
+                    )}
                   </FormControl>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth size='medium'>
-                    <InputLabel htmlFor='link-surf'>Link Surf</InputLabel>
-                    <OutlinedInput label='Link Surf' id='link-surf' placeholder='Link do Spec da Surf' />
+                  <FormControl fullWidth size='medium' error={!!errors.link}>
+                    <InputLabel htmlFor={tipoProtocolo === 'surf' ? 'link-surf' : 'link-auvo'}>
+                      {tipoProtocolo === 'surf' ? 'Link Surf*' : 'Link Auvo*'}
+                    </InputLabel>
+                    <OutlinedInput
+                      label={tipoProtocolo === 'surf' ? 'Link Surf*' : 'Link Auvo*'}
+                      id={`${tipoProtocolo === 'surf' ? 'link-surf' : 'link-auvo'}`}
+                      placeholder={tipoProtocolo === 'surf' ? 'Link do Spec da Surf' : 'Link do Ticket do Auvo'}
+                      {...register('link')}
+                    />
+                    {!!errors.link && (
+                      <FormHelperText sx={{ color: theme => theme.palette.error.main }}>
+                        {tipoProtocolo === 'surf' ? 'Link Surf' : 'Link Auvo'} {errors.link.message}
+                      </FormHelperText>
+                    )}
                   </FormControl>
                 </Grid>
 
--- src/views/ui/cards/widgets/CardCustomBloqueioLinha.tsx
+++ src/views/ui/cards/widgets/CardCustomBloqueioLinha.tsx
@@ -5,9 +5,33 @@ import CardHeader from '@mui/material/CardHeader'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { FormControlLabel, Grid, Radio, RadioGroup } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const SchemaBloquearDesbloquearForm = z.object({
+  voz: z.enum(['bloquear', 'desbloquear']),
+  sms: z.enum(['bloquear', 'desbloquear']),
+  dados: z.enum(['bloquear', 'desbloquear', 'reduzir'])
+})
+
+type BloquearDesbloquearFormData = z.infer<typeof SchemaBloquearDesbloquearForm>
 
 const CardCustomBloqueioLinha = () => {
   // ** Hook
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<BloquearDesbloquearFormData>({
+    resolver: zodResolver(SchemaBloquearDesbloquearForm)
+  })
+
+  async function handleSubmitBloquearDesbloquearForm(data: BloquearDesbloquearFormData) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
 
   return (
     <Card>
@@ -17,31 +41,70 @@ const CardCustomBloqueioLinha = () => {
           sx: { lineHeight: '1 rem !important', letterSpacing: '0.15px !important' }
         }}
       />
-      <CardContent sx={{ '& .apexcharts-xcrosshairs.apexcharts-active': { opacity: 0 } }}>
+      <CardContent
+        sx={{ '& .apexcharts-xcrosshairs.apexcharts-active': { opacity: 0 } }}
+        component='form'
+        onSubmit={handleSubmit(handleSubmitBloquearDesbloquearForm)}
+      >
         <Grid item xs={12}>
           <Typography variant='body2'>Voz - Origina</Typography>
-          <RadioGroup row aria-label='voz' name='voz' defaultValue='voz-unchecked'>
-            <FormControlLabel value='voz-checked' control={<Radio />} label='Bloquear' />
-            <FormControlLabel value='voz-unchecked' control={<Radio />} label='Desbloquear' />
+          <RadioGroup row aria-label='voz' defaultValue='desbloquear'>
+            <FormControlLabel
+              value='bloquear'
+              control={<Radio color='error' />}
+              {...register('voz')}
+              label='Bloquear'
+            />
+            <FormControlLabel
+              value='desbloquear'
+              control={<Radio color='success' />}
+              {...register('voz')}
+              label='Desbloquear'
+            />
           </RadioGroup>
         </Grid>
         <Grid item xs={12}>
           <Typography variant='body2'>SMS</Typography>
-          <RadioGroup row aria-label='sms' name='sms' defaultValue='sms-unchecked'>
-            <FormControlLabel value='sms-checked' control={<Radio />} label='Bloquear' />
-            <FormControlLabel value='sms-unchecked' control={<Radio />} label='Desbloquear' />
+          <RadioGroup row aria-label='sms' defaultValue='desbloquear'>
+            <FormControlLabel
+              value='bloquear'
+              control={<Radio color='error' />}
+              {...register('sms')}
+              label='Bloquear'
+            />
+            <FormControlLabel
+              value='desbloquear'
+              control={<Radio color='success' />}
+              {...register('sms')}
+              label='Desbloquear'
+            />
           </RadioGroup>
         </Grid>
         <Grid item xs={12}>
           <Typography variant='body2'>Dados</Typography>
-          <RadioGroup row aria-label='dados' name='dados' defaultValue='dados-unchecked'>
-            <FormControlLabel value='dados-checked' control={<Radio />} label='Bloquear' />
-            <FormControlLabel value='dados-unchecked' control={<Radio />} label='Desbloquear' />
-            <FormControlLabel value='dados-reduced' control={<Radio />} label='Reduzir' />
+          <RadioGroup row aria-label='dados' defaultValue='desbloquear'>
+            <FormControlLabel
+              value='bloquear'
+              control={<Radio color='error' />}
+              {...register('dados')}
+              label='Bloquear'
+            />
+            <FormControlLabel
+              value='desbloquear'
+              control={<Radio color='success' />}
+              {...register('dados')}
+              label='Desbloquear'
+            />
+            <FormControlLabel
+              value='reduzir'
+              control={<Radio color='warning' />}
+              {...register('dados')}
+              label='Reduzir'
+            />
           </RadioGroup>
         </Grid>
-        <Button fullWidth variant='contained' sx={{ mt: 4 }}>
-          Atualizar
+        <Button fullWidth variant='contained' sx={{ mt: 4 }} type='submit' disabled={isSubmitting}>
+          {isSubmitting ? 'Atualizando...' : 'Atualizar'}
         </Button>
       </CardContent>
     </Card>
